db3bf916aad551680ab46b5be68ad5f51a5c8a48
Thomas Thomassen <thomas@thomthom.net>
2013-05-29 15:27:06 +0200

    Added class Container

---
+++ src/SKUI/container.rb
@@ -0,0 +1,16 @@
+module SKUI
+
+  require File.join( PATH, 'control.rb' )
+
+
+  # Container control grouping child controls.
+  #
+  # @since 1.0.0
+  class Container < Control
+
+    include ContainerElement
+
+    # (!) Background color. (Style)
+
+  end # class
+end # module
