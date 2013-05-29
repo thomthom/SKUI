9f543900f74c1c4779ed3dc2985bc6b5d0808c6a
Thomas Thomassen <thomas@thomthom.net>
2013-05-29 16:29:21 +0200

    Added class Groupbox

---
+++ src/SKUI/groupbox.rb
@@ -0,0 +1,13 @@
+module SKUI
+
+  require File.join( PATH, 'control.rb' )
+
+
+  # @since 1.0.0
+  class Groupbox < Container
+
+    # @since 1.0.0
+    prop( :label )
+
+  end # class
+end # module
